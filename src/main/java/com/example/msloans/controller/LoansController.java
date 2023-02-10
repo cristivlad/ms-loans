a8a379bc0d7c77a5471ec2559fd9905507cb13f9
cristiii <cristian.vlad10@gmail.com>
2023-02-10 19:39:20 +0200

    backbone classes

---
+++ src/main/java/com/example/msloans/controller/LoansController.java
@@ -0,0 +1,23 @@
+package com.example.msloans.controller;
+
+import com.example.msloans.model.Customer;
+import com.example.msloans.model.Loans;
+import com.example.msloans.repository.LoansRepository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LoansController {
+
+    private final LoansRepository loansRepository;
+
+    public LoansController(LoansRepository loansRepository) {
+        this.loansRepository = loansRepository;
+    }
+
+    public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+        return loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+    }
+}
---
+++ src/main/java/com/example/msloans/model/Loans.java
@@ -0,0 +1,29 @@
+package com.example.msloans.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Date;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Loans {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int loanNumber;
+    private int customerId;
+    private Date startDt;
+    private String loanType;
+    private int totalLoan;
+    private int amountPaid;
+    private int outstandingAmount;
+    private String createDt;
+}
